563c27f3edf4579496dda16362263a9f8d1cbdf5
tomaszgaj <48648002+tomaszgaj@users.noreply.github.com>
2020-10-24 01:38:43 +0200

    Initial commit

---
+++ Models/CompanyAddedViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    public class CompanyAddedViewModel: CompanyModel
+    {
+        public int NumberOfCharsInName { get; set; }
+
+        public int NumberOfCharsInDescription { get; set; }
+
+        public bool IsHidden { get; set; }
+        
+    }
+}
---
+++ Models/CompanyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    public class CompanyModel
+    {
+        public string Name {get; set;}
+
+        public string Description { get; set; }
+
+
+        public bool IsVisible { get; set; }
+    }
+}
